81910cfdfb17fa9f651b6183e65abc79039c2fb4
tbressel <tbressel.dev@gmail.com>
2023-07-25 02:31:16 +0200

    Correction du 1er exercice sur le même thème. Rajoute du responsif Desktop ainsi que l'ajout que quelques animation

---
+++ correction/assets/js/script.js
@@ -0,0 +1,6 @@
+        var UNTEST = document.getElementById("test")
+        UNTEST.addEventListener("click", () => {
+                             var baliseAudio = document.getElementById("audio");
+                             baliseAudio.play();
+            })
+    
