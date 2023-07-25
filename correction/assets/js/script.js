4dd4eef5c1203597d00dc27dbb24b868a62f9777
tbressel <tbressel.dev@gmail.com>
2023-07-25 09:25:31 +0200

    Rajouter d'un sample bébé

--- correction/assets/js/script.js
+++ correction/assets/js/script.js
@@ -4,3 +4,9 @@
                              baliseAudio.play();
             })
     
+        var UNTEST = document.getElementById("test2")
+        UNTEST.addEventListener("click", () => {
+                             var baliseAudio = document.getElementById("audio2");
+                             baliseAudio.play();
+            })
+    
