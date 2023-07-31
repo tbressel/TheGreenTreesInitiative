6b9bcdb62f196279e1a2d96205574c181a4384d7
tbressel <tbressel.dev@gmail.com>
2023-07-31 17:32:16 +0200

    Fin de l'exercice HTML - CSS. 2 points de responsivité, 1 en desktop et 1 en mobile. Modification de tous les textes, et remplacement de toute les photos par d'autres libre de droit sous réserve de mentionner l'auteur comme indiqué.

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+        var UNTEST = document.getElementById("test")
+        UNTEST.addEventListener("click", () => {
+                             var baliseAudio = document.getElementById("audio");
+                             baliseAudio.play();
+            })
