4c50b75801799f47cf37936f2c0620aa63da1da3
tbressel <tbressel.dev@gmail.com>
2023-08-02 02:21:34 +0200

    Ajout d'un menu burger en version mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,14 @@
                              var baliseAudio = document.getElementById("audio");
                              baliseAudio.play();
             })
+
+
+document.getElementById("menu_burger").addEventListener('click', () => {
+document.querySelector(".navigation").classList.toggle('menu_on');
+
+const ELEMENT__burger = document.querySelectorAll(".line");
+
+
+
+
+});
